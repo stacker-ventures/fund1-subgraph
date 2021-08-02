183cf0322ccd24132f00cab6e82726f30d5fc4c7
Random Developer <yourusualrandomdeveloper@gmail.com>
2021-08-01 20:34:27 -0400

    Resets project record

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,77 +1,42 @@
 import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  GaugeD1,
-  Deposit,
-  STACKClaimed,
-  Upgrade,
-  Withdraw
-} from "../generated/GaugeD1/GaugeD1"
-import { ExampleEntity } from "../generated/schema"
+
+import { Fund1Commitment } from "../generated/schema"
+import { Deposit, Withdraw } from "../generated/GaugeD1/GaugeD1"
+
+
+let zero = BigInt.fromI32(0)
+
 
 export function handleDeposit(event: Deposit): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let depositor = event.params.from.toHex()
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (event.params.amountCommitHard.gt(zero) || event.params.amountCommitSoft.gt(zero)) {
+    let fundCommitment = Fund1Commitment.load(depositor)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    if (fundCommitment == null) {
+      fundCommitment = new Fund1Commitment(depositor)
+
+      fundCommitment.softAmount = event.params.amountCommitSoft
+      fundCommitment.hardAmount = event.params.amountCommitHard
+    } else {
+      fundCommitment.softAmount = fundCommitment.softAmount.plus(event.params.amountCommitSoft)
+      fundCommitment.hardAmount = fundCommitment.hardAmount.plus(event.params.amountCommitHard)
+    }
+
+    fundCommitment.save()
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.from = event.params.from
-  entity.amountCommitSoft = event.params.amountCommitSoft
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.STACK(...)
-  // - contract.acceptToken(...)
-  // - contract.balances(...)
-  // - contract.claimSTACK(...)
-  // - contract.commitHardWeight(...)
-  // - contract.commitSoftWeight(...)
-  // - contract.depositedCommitHard(...)
-  // - contract.depositedCommitSoft(...)
-  // - contract.emissionRate(...)
-  // - contract.endBlock(...)
-  // - contract.fundOpen(...)
-  // - contract.getCommitted(...)
-  // - contract.getTotalBalance(...)
-  // - contract.getTotalWeight(...)
-  // - contract.getUserBalance(...)
-  // - contract.getUserWeight(...)
-  // - contract.governance(...)
-  // - contract.lastBlock(...)
-  // - contract.startBlock(...)
-  // - contract.tokensAccrued(...)
-  // - contract.vaultGaugeBridge(...)
 }
 
-export function handleSTACKClaimed(event: STACKClaimed): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let depositor = event.params.to.toHex()
 
-export function handleUpgrade(event: Upgrade): void {}
+  let fundCommitment = Fund1Commitment.load(depositor)
 
-export function handleWithdraw(event: Withdraw): void {}
+  if (fundCommitment == null) {
+    return
+  }
+
+  fundCommitment.softAmount = fundCommitment.softAmount.minus(event.params.amount)
+
+  fundCommitment.save()
+}
